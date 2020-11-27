fd7d0367292131e1370c39bf3e9b857a42f60c32
Arif Suwadji <johny.adji@gmail.com>
2020-11-27 17:27:56 +0700

    form crud sub

--- core/crud_sub.php
+++ core/crud_sub.php
@@ -14,6 +14,7 @@
 
         $string .="use tide::{Request, Response};
 use sqlx::PgPool;
+use crate::{data, data::{fields, Type}};
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 struct ".ucfirst($table)." {
@@ -37,6 +38,7 @@ struct ".ucfirst($table)." {
         $i++;
     }
     $string .="
+    #[serde(skip_deserializing)]
 }
 
 #[derive(serde::Deserialize)]
@@ -90,7 +92,52 @@ pub async fn hapus(req: Request<PgPool>) -> tide::Result<Response> {
 
     crate::ws_response(\"OK\", \"Data telah dihapus\")
 }
+";
+$string .="
+pub async fn form(req: Request<PgPool>) -> tide::Result<Body> {
+    let pool = req.state();
+    fields(vec![
         ";
+    $arrForeigns = explode(';', $foreigns);
+    foreach($allField as $fieldName){
+        // var_dump($arrForeigns);
+        $found_string = find_string($fieldName['column_name'], $arrForeigns);
+        // echo($found_string);
+        if($found_string){
+    $string .="(\"".$fieldName['column_name']."\", Type::Select, data::".$fieldName['column_name']."(pool).await?),
+    ";
+        }else{
+            if($fieldName['data_type'] == 'USER-DEFINED'){
+    $string .="(\"".$fieldName['column_name']."\", Type::Select, data::".$fieldName['column_name']."()),
+    ";
+            }else if($fieldName['data_type'] == 'json'){
+    $string .="(\"".$fieldName['column_name']."\", Type::Number, vec![]),
+    ";
+    $string .="(\"jenisbayar_".$fieldName['column_name']."\", Type::Select, data::jenis_pembayaran()),
+    ";
+            }else{
+                if($fieldName['column_name'] == 'id'){
+    $string .="(\"".$fieldName['column_name']."\", Type::Number, data::auto_inc()),
+    ";
+                }else{
+                    if($fieldName['data_type'] == 'integer'){
+    $string .="(\"".$fieldName['column_name']."\", Type::Number, vec![]),
+    ";
+                    }else if($fieldName['data_type'] == 'smallint'){
+    $string .="(\"".$fieldName['column_name']."\", Type::Number, vec![]),
+    ";
+                    }else{
+    $string .="(\"".$fieldName['column_name']."\", Type::Text, vec![]),
+    ";
+                    }
+                }
+            }
+        }
+    }
+    $string .="
+    ])
+}
+";
         //controller
         createFile($string, BASE_PATH."/src/handler/$folder1/$folder2/".$table.".rs");
         
